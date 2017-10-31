777a9a9fd43e0759bc9a776a51db70b288fc3389
mvmullin <mvm5854@rit.edu>
2017-10-31 15:10:28 -0400

    bundled

--- hosted/bundle.js
+++ hosted/bundle.js
@@ -1,28 +1,29 @@
 'use strict';
 
-var id = void 0; // player's unique id
-var color = void 0; // player's unique color
-var socket = void 0; // player's socket
-var players = {}; // object to hold player properties
-var ingrediants = {}; // object to hold falling ingrediants
-var ingrediantNum = 0;
-var moveLeft = false; // left or a held
-var moveRight = false; // right or d held
+var arrow1 = void 0; // this client's arrow
+var arrow2 = void 0; // player 2's arrow
+var socket = void 0; // arrow's socket
+
+var mouse = {
+  down: false,
+  start: { x: 0, y: 0 },
+  end: { x: 0, y: 0 }
+};
+
+var mouseStartX = 0;
+var mouseStartY = 0;
+var mouseEndX = 0;
+var mouseEndY = 0;
 
 var canvas = void 0;
 var ctx = void 0;
 
-// bread properties
-var breadHeight = 10;
-var breadWidth = 30;
-
 //redraw canvas
 var draw = function draw() {
-  movePlayer(); // get player movement
-
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clear screen
 
-  drawPlayers();
+  drawArrow(arrow1, '#4dc3ff', false);
+  drawArrow(arrow2, '#ff4d4d', true);
   requestAnimationFrame(draw); // continue to draw updates
 };
 
@@ -31,28 +32,29 @@ var lerp = function lerp(v0, v1, alpha) {
   return (1 - alpha) * v0 + alpha * v1;
 };
 
-// object to keep track of keys that are down
-var keysDown = {};
-// function to update player movement based on keys down.
-var keyDownHandler = function keyDownHandler(e) {
-  e = e || event;
+// function to update position of initial arrow draw
+var mouseDownHandler = function mouseDownHandler(e) {
+  mouse.down = true;
+  if (arrow1.state === arrowState.READY) arrow1.state = arrowState.DRAWN;
+  mouse.start.x = e.pageX;
+  mouse.start.y = e.pageY;
+};
 
-  // If key isn't held check press commands
-  if (!keysDown[e.keyCode]) {
-    switch (e.keyCode) {
-      case 32:
-        // space
-        jump();
-        break;
-      default:
-        break;
-    }
+// function to track arrow draw power
+var mouseMoveHandler = function mouseMoveHandler(e) {
+  if (mouse.down) {
+    mouse.end.x = e.pageX;
+    mouse.end.y = e.pageY;
   }
+};
 
-  keysDown[e.keyCode] = e.type == 'keydown'; // check if key is down
-
-  moveLeft = keysDown[37] || keysDown[65]; // left or a held
-  moveRight = keysDown[39] || keysDown[68]; // right or d held
+// function to fire a drawn arrow
+var mouseUpHandler = function mouseUpHandler(e) {
+  mouse.down = false;
+  if (arrow1.state === arrowState.DRAWN) {
+    arrow1.state = arrowState.FIRED;
+    socket.emit('fireArrow', mouse);
+  }
 };
 
 // initialize scripts
@@ -66,112 +68,59 @@ var init = function init() {
     socket.emit('join', { width: canvas.width, height: canvas.height });
   });
 
-  socket.on('joined', setPlayer); // set player on server 'joined' event
-  socket.on('updateMovement', updatePlayer); // update on server 'updateClient' event
-  socket.on('applyGravity', applyGravity);
-  socket.on('left', removePlayer); // remove player on server 'removePlayer event
+  socket.on('joined', setArrow); // set arrow on server 'joined' event
+  socket.on('updateArrows', updateArrows); // update on server 'updateClient' event
+  socket.on('left', removeArrow); // remove arrow on server 'removeArrow event
 
-  document.body.addEventListener('keydown', keyDownHandler);
-  document.body.addEventListener('keyup', keyDownHandler);
+  document.body.addEventListener('mouseDown', mouseDownHandler);
+  document.body.addEventListener('mouseMove', mouseMoveHandler);
+  document.body.addEventListener('mouseUp', mouseUpHandler);
 };
 
 window.onload = init;
-'use strict';
+"use strict";
 
-var drawPlayers = function drawPlayers() {
-  var keys = Object.keys(players); // get all player id's
+var arrowState = {
+  READY: 1,
+  DRAWN: 2,
+  FIRED: 3
+};
 
-  // Iterate players
-  for (var i = 0; i < keys.length; i++) {
-    var player = players[keys[i]];
+var drawArrow = function drawArrow(arrow, color, player2) {
+  // keep animation running smoothly
+  if (arrow.alpha < 1) arrow.alpha += 0.05;
 
-    // keep animation running smoothly
-    if (player.alpha < 1) player.alpha += 0.05;
+  // set draw color to unique arrow color
+  ctx.fillStyle = color;
 
-    // set draw color to unique player color
-    ctx.fillStyle = player.color;
+  arrow.x = arrow.state != arrowState.FIRED ? arrow.destX : lerp(arrow.prevX, arrow.destX, arrow.alpha); // smooth transition with lerp
+  arrow.y = arrow.state != arrowState.FIRED ? arrow.destY : lerp(arrow.prevY, arrow.destY, arrow.alpha);
 
-    player.x = player.destX == 0 || player.destX == canvas.width ? player.destX : lerp(player.prevX, player.destX, player.alpha); // smooth transition with lerp
-    player.y = lerp(player.prevY, player.destY, player.alpha);
+  var path = new Path2D();
+  path.moveTo(arrow.x, arrow.y);
+  path.lineTo(arrow.x - 10, arrow.y - 1);
+  path.lineTo(arrow.x - 10, arrow.y + 1);
+  ctx.fill(path);
+};
 
-    ctx.fillRect(player.x, player.y, player.width, player.height);
+var lastUpdate = 0;
+// update a arrow from server
+var updateArrows = function updateArrows(data) {
+  if (data.lastUpdate > lastUpdate) {
+    var curX = arrow1.x;
+    var curY = arrow1.y;
+
+    arrow1 = data.arrows[arrow1.id];
+    arrow1.x = curX;
+    arrow1.y = curY;
+
+    // reset lerp percentage
+    arrow.alpha = 0.05;
   }
 };
 
-// update a player from server
-var updatePlayer = function updatePlayer(data) {
-  // if the player is new, add them to players and return
-  if (!players[data.id]) {
-    players[data.id] = data;
-    return;
-  }
-
-  // grab player object based on id
-  var player = players[data.id];
-
-  // return if player's last update is newer than this server data
-  if (player.lastUpdate >= data.lastUpdate) {
-    return;
-  }
-
-  //update positions to lerp between
-  player.prevX = data.prevX;
-  player.prevY = data.prevY;
-  player.destX = data.destX;
-  player.destY = data.destY;
-  player.ySpeed = data.ySpeed;
-
-  // reset lerp percentage
-  player.alpha = 0.05;
-};
-
-var applyGravity = function applyGravity(data) {
-  var serverKeys = Object.keys(data);
-
-  for (var i = 0; i < serverKeys.length; i++) {
-    if (players[data[serverKeys[i]].id]) updatePlayer(data[serverKeys[i]]);
-  }
-};
-
-// remove player based on id
-var removePlayer = function removePlayer(id) {
-  if (players[id]) {
-    delete players[id];
-  }
-};
-
-// set this player from server
-var setPlayer = function setPlayer(data) {
-  id = data.id; // set id from server data
-  color = data.color; // set color from server data
-  players[id] = data; // set player with new id
-  requestAnimationFrame(draw); // draw with new info
-};
-
-// function to update player position
-var movePlayer = function movePlayer() {
-  var player = players[id]; // get this player with their id
-  player.prevX = player.x;
-
-  var speed = 5; // how far to move
-
-  if (moveLeft) {
-    if (player.destX < 0) player.destX = canvas.width;else player.destX -= speed;
-  }
-  if (moveRight) {
-    if (player.destX > canvas.width) player.destX = 0;else player.destX += speed;
-  }
-
-  if (player.destY > canvas.height - player.height) player.destY = canvas.height - player.height;
-
-  // reset alpha when moving to keep playing animation
-  player.alpha = 0.05;
-
-  // send movement to server
-  socket.emit('move', player);
-};
-
-// add to player's speed on jump
-var jump = function jump() {
-  if (players[id].destY >= canvas.height - players[id].height) socket.emit('jump');
+// remove arrow based on id
+var setArrow = function setArrow(data) {
+  arrow1 = data;
+  arrow1.id = data.id;
 };
